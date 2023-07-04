7d6d7364e3678de4f7bb2af5a97767b3f4152061
lcn <lcniell@gmail.com>
2023-07-04 08:47:20 -0700

    update testing

--- src/test/java/com/gildedrose/LowerValuesPerDayTest.java
+++ src/test/java/com/gildedrose/LowerValuesPerDayTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GildedRoseTest {
+class LowerValuesPerDayTest {
 
     @Test
     void foo() {
--- src/test/java/com/gildedrose/SellInValueTest.java
+++ src/test/java/com/gildedrose/SellInValueTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GildedRoseTest {
+class SellInValueTest {
 
     @Test
     void foo() {
